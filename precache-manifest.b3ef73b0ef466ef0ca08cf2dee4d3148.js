52e039ba35f5f93e78b1966ec55201eeb4ee598b
Alasdair Moffat <contact@alasdairmoffat.com>
2019-10-20 15:29:01 +0100

    Updates

---
+++ precache-manifest.b3ef73b0ef466ef0ca08cf2dee4d3148.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "141c20b4a8347fa9195de2dcd9a586bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "559c2abe22382577f141",
+    "url": "/static/css/main.f2e7741f.chunk.css"
+  },
+  {
+    "revision": "38b18826de3f63cc9511",
+    "url": "/static/js/2.dab1d908.chunk.js"
+  },
+  {
+    "revision": "559c2abe22382577f141",
+    "url": "/static/js/main.2e0d4b15.chunk.js"
+  },
+  {
+    "revision": "cc5f1e3fa0960e22ed39",
+    "url": "/static/js/runtime-main.6c53bc9c.js"
+  }
+]);
